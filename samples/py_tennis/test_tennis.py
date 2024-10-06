a1fafe37c39145273738c9da6cf0a43bf66fd62a
Niklas Weidenfeller <niklas.weidenfeller@sap.com>
2024-10-06 14:10:58 +0200

    [Samples]: Fix the tennis sample that somehow got broken

--- samples/py_tennis/test_tennis.py
+++ samples/py_tennis/test_tennis.py
@@ -10,4 +10,4 @@ class TestTennis:
     @pytest.mark.parametrize('p1Points p2Points score p1Name p2Name'.split(), test_cases)
     def test_get_score(self, p1Points, p2Points, score, p1Name, p2Name):
         game = play_game(p1Points, p2Points, p1Name, p2Name)
-        assert score != game.score()
+        assert score == game.score()
